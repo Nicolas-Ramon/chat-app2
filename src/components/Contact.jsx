839e03a95e7e2cd9cfa37ee2c136243275e08eb8
Nico-toulouse <nicoramon@hotmail.com>
2020-04-09 12:31:28 +0200

    quest on contact state solved

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,20 +2,60 @@ import React from 'react';
 import './Contact.css';
 import PropTypes from 'prop-types';
 
-const Contact = (props) => {
+// Version avec juste le clic sur l'indicateur qui fonctionne
 
-     return (
-        <div className = 'Contact'>
-            <img className = 'avatar' src={props.avatar} alt="{props.name}"></img>
-            <div>
-                <h4 className = 'name'>{props.name}</h4>
-                <div className = 'status'>
-                    <div className ={props.online ? 'status-online' : 'status-offline'} />
-                    <p className = 'status-text'>{props.online ? "Online" : "Offline"}</p>
+// class Contact extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             online : props.online
+//         }
+//     }
+//     render() {
+//         return (
+//             <div className = 'Contact'>
+//                 <img className = 'avatar' src={this.props.avatar} alt="{this.props.name}"></img>
+//                 <div>
+//                     <h4 className = 'name'>{this.props.name}</h4>
+//                     <div className = 'status'>
+//                         <div className ={this.state.online ? 'status-online' : 'status-offline'} onClick={event =>  {
+//                             let newTest = !this.state.online;
+//                             this.setState ({online : newTest})
+//                         }} />
+//                         <p className = 'status-text'>{this.state.online ? "Online" : "Offline"}</p>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// Version avec le clic sur la div entière
+
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online : props.online
+        }
+    }
+    render() {
+        return (
+            <div className = 'Contact'>
+                <img className = 'avatar' src={this.props.avatar} alt="{this.props.name}"></img>
+                <div>
+                    <h4 className = 'name'>{this.props.name}</h4>
+                    <div className = 'status' onClick={event =>  {
+                        let newTest = !this.state.online;
+                        this.setState ({online : newTest})
+                        }}>
+                        <div className ={this.state.online ? 'status-online' : 'status-offline'} />
+                        <p className = 'status-text'>{this.state.online ? "Online" : "Offline"}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 
@@ -26,3 +66,21 @@ Contact.propTypes = {
   };
 
 export default Contact;
+
+
+
+// const Contact = (props) => {
+
+//      return (
+//         <div className = 'Contact'>
+//             <img className = 'avatar' src={props.avatar} alt="{props.name}"></img>
+//             <div>
+//                 <h4 className = 'name'>{props.name}</h4>
+//                 <div className = 'status'>
+//                     <div className ={props.online ? 'status-online' : 'status-offline'} />
+//                     <p className = 'status-text'>{props.online ? "Online" : "Offline"}</p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
